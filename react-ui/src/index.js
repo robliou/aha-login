8f21fb74db2a2c8cd61c92018d7c441819b2fa44
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-08-22 17:55:59 +0800

    Update index.js

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -23,10 +23,10 @@ root.render(
     domain="dev-7-8i89hb.us.auth0.com"
     /*      clientId="6J2cpQGzD456WzodmDHXj4Kot4y84bgI"
      */ clientId="yABzJ5U03ynSjSijYOEWkwymAt84CFDS"
-    /* redirectUri="https://aha-login.herokuapp.com" */
+    redirectUri="https://aha-login.herokuapp.com"
     //Note- unfreeze above and freeze below when going from production to local testing!
-    redirectUri="http://localhost:3000/home"
-    audience="https://dev-7-8i89hb.us.auth0.com/api/v2/"
+    /*     redirectUri="http://localhost:3000/home"
+     */ audience="https://dev-7-8i89hb.us.auth0.com/api/v2/"
     cacheLocation={"localstorage"}
     /*     audience="hasura"
      */
