a5be1b8163bfb33010447476fdfd74072eb021ba
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-08-23 16:42:52 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,10 +33,10 @@ const jwtCheck = jwt({
  */
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "../build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build"));
+    res.sendFile(path.join(__dirname, "../build"));
   });
 }
 app.use(cors(corsOptions));
