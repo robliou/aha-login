9abb7907fcccbb7b1f3f372a2ccac42939f03548
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-08-23 16:11:12 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,9 +36,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.basename("/app/build/index.html")));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.basename("C:/Users/Rob/projects/aha-login/buildindex.html")
-    );
+    res.sendFile(path.resolve(__dirname, ".../build"));
   });
 }
 app.use(cors(corsOptions));
