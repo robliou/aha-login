95ff0cbe8321e16f3c63bf9cdb9717b911308cc3
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-08-25 23:22:34 +0800

    Update ChangeName.js

--- react-ui/src/components/ChangeName.js
+++ react-ui/src/components/ChangeName.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./../styles/changeName.css";
+import { useState } from "react";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
