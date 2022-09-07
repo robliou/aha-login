48199fbb091f8e9e9dcd0317b53fdbd4d6d936e6
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-09-07 11:26:40 +0800

    Home page style improvements

--- react-ui/src/components/Root.js
+++ react-ui/src/components/Root.js
@@ -22,7 +22,7 @@ const Root = () => {
           <strong>
             {" "}
             Welcome to Rob's <br></br> Aha! Login Demo. <br></br>Please Login or
-            Sign-up &#128512 <br />{" "}
+            Sign-up &#41; <br />{" "}
           </strong>
         </h1>
       </div>
